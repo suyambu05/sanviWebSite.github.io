b2285e2e41d559be59b219f0553229f75d9793e8
Suyambu <suyambu-rajan.athilingam-renexter@rntbci.com>
2017-04-22 17:33:46 +0530

    Map

--- js/main.js
+++ js/main.js
@@ -185,4 +185,80 @@ jQuery(function($) {'use strict',
 
 });
 
+// Google Map Customization
+(function(){
 
+	var map;
+
+	map = new GMaps({
+		el: '#gmap',
+		lat: 8.189224,
+		lng: 77.707017,
+		scrollwheel:false,
+		zoom: 17,
+		zoomControl : true,
+		panControl : false,
+		streetViewControl : false,
+		mapTypeControl: true,
+		overviewMapControl: false,
+		clickable: false
+	});
+
+	var image = 'images/map-icon.png';
+	map.addMarker({
+		lat: 8.189224,
+		lng: 77.707017,
+		icon: image,
+		animation: google.maps.Animation.DROP,
+		verticalAlign: 'bottom',
+		horizontalAlign: 'center',
+		backgroundColor: '#3e8bff',
+	});
+
+
+	var styles = [ 
+
+	{
+		"featureType": "road",
+		"stylers": [
+		{ "color": "#b4b4b4" }
+		]
+	},{
+		"featureType": "water",
+		"stylers": [
+		{ "color": "#d8d8d8" }
+		]
+	},{
+		"featureType": "landscape",
+		"stylers": [
+		{ "color": "#f1f1f1" }
+		]
+	},{
+		"elementType": "labels.text.fill",
+		"stylers": [
+		{ "color": "#000000" }
+		]
+	},{
+		"featureType": "poi",
+		"stylers": [
+		{ "color": "#d9d9d9" }
+		]
+	},{
+		"elementType": "labels.text",
+		"stylers": [
+		{ "saturation": 1 },
+		{ "weight": 0.1 },
+		{ "color": "#000000" }
+		]
+	}
+
+	];
+
+	map.addStyle({
+		styledMapName:"Styled Map",
+		styles: styles,
+		mapTypeId: "map_style"  
+	});
+
+	map.setStyle("map_style");
+}());
